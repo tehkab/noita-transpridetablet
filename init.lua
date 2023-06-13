4044fd109310119d0c0fe17d8ab56e95d9ec5eaf
Kabby <1925023+tehkab@users.noreply.github.com>
2023-06-12 20:51:49 -0400

    Add appropriate colored glimmer to tablet This adds a new material similar to rainbow liquid, with trans colors Also fix incorrect file reference missed in original push

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 local content = ModTextFileGetContent("mods/transpridetablet/data/entities/items/books/base_bookPURP.xml")
 ModTextFileSetContent("data/entities/items/books/base_book.xml", content)
+ModMaterialsFileAdd("mods/transpridetablet/data/transmaterial.xml")
+
+local content = ModTextFileGetContent("data/translations/common.csv")
+ModTextFileSetContent("data/translations/common.csv", content .. [[
+    mat_trans_pride, Trans Pride,
+]])
